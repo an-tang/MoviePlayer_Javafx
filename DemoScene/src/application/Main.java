dc59aa1fc3a527ca2b7d5540cc5389f045d86168
hoangan1903 <antanghoang@gmail.com>
2019-04-11 21:40:17 +0700

    slider time, button control

--- DemoScene/src/application/Main.java
+++ DemoScene/src/application/Main.java
@@ -1,29 +1,50 @@
 package application;
 
+import java.awt.Color;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
-	
-	@Override
 
-		public void start(Stage primaryStage){
-			try {
-				Parent root = FXMLLoader.load(getClass().getResource("MyScene.fxml"));
-				Scene scene = new Scene(root);
-				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-				primaryStage.setTitle("Movie Explorer");
-				primaryStage.setScene(scene);
-				primaryStage.show();
-			}catch(Exception e) {
-				e.printStackTrace();
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MyScene.fxml"));
+			Scene scene = new Scene(root);
+
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+			scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					if (event.getClickCount() == 2) {
+						primaryStage.setFullScreen(true);
+					} else
+						primaryStage.setFullScreen(false);
+				}
+			});
+			primaryStage.setTitle("Movie Explorer");
+			// primaryStage.setFullScreen(true);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- DemoScene/src/application/MySceneController.java
+++ DemoScene/src/application/MySceneController.java
@@ -8,54 +8,91 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.util.Duration;
+import javafx.util.converter.NumberStringConverter;
 import javafx.beans.binding.Bindings;
 
 public class MySceneController implements Initializable {
 	@FXML private MediaView mv;
 	@FXML private Slider sliderTime;
 	@FXML private Slider slVolume;
-	private MediaPlayer mediapplayer;
-	private Media media;
+	@FXML private BorderPane mainPane;
+	@FXML private Label lbTime;
+	@FXML private Label lbMaxTime;
+	private MediaPlayer mediaplayer;
+
 	private double rate = 1;
 	private String filePath;
+	
 	@FXML
 	private void handleButtonAction(ActionEvent event) throws MalformedURLException{
 		FileChooser filechooser = new FileChooser();
-		FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Select only mp4 file", "*.mp4", "*.mkv");
+		FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Select only mp4 file", "*.mp4", "*.mp3");
 		filechooser.getExtensionFilters().add(filter);
 		File file = filechooser.showOpenDialog(null);
+//		AnchorPane.setTopAnchor(mainPane, 0.0);
+//		AnchorPane.setLeftAnchor(mainPane, 0.0);
+//		AnchorPane.setRightAnchor(mainPane, 0.0);
+//		AnchorPane.setBottomAnchor(mainPane, 0.0);
 		filePath = file.toURI().toURL().toString();
+		
 		if(filePath!=null) {
-			media = new Media(filePath);
-			mediapplayer = new MediaPlayer(media);
-			mv.setMediaPlayer(mediapplayer);
-			mediapplayer.setAutoPlay(true);
+			Media media = new Media(filePath);
+			mediaplayer = new MediaPlayer(media);
+			
+			mediaplayer.setAutoPlay(true);
+			mv.setMediaPlayer(mediaplayer);
 			DoubleProperty width = mv.fitWidthProperty();
-			DoubleProperty height = mv.fitHeightProperty();
+			DoubleProperty height = mv.fitHeightProperty();		
 			width.bind(Bindings.selectDouble(mv.sceneProperty(), "width"));
 			height.bind(Bindings.selectDouble(mv.sceneProperty(), "height"));
-			mediapplayer.setCycleCount(MediaPlayer.INDEFINITE);
-			mediapplayer.play();
-			sliderTime.setMin(0.0);
-			sliderTime.setValue(0.0);
-			sliderTime.setMax(mediapplayer.getTotalDuration().toSeconds());
 			
 			
-			mediapplayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+			//mediapplayer.setCycleCount(MediaPlayer.INDEFINITE);
+			mediaplayer.play();
+			mediaplayer.setOnReady(new Runnable() {
+				
+				@Override
+				public void run() {
+					sliderTime.setMaxWidth(mediaplayer.getMedia().getWidth()-100);
+					sliderTime.setMin(0.0);
+					sliderTime.setValue(0.0);
+					sliderTime.setMax(mediaplayer.getTotalDuration().toSeconds());
+					double maxTime = mediaplayer.getTotalDuration().toSeconds();
+					lbMaxTime.setText(convertTime((String.valueOf(maxTime))));
+					
+				}
+			});
+			
+			mediaplayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
 				@Override
 				public void changed(ObservableValue<? extends Duration> observableValue, Duration duration, Duration current) {
 					sliderTime.setValue(current.toSeconds());
+					lbTime.setText(convertTime(String.valueOf(sliderTime.getValue())));
+				}
+			});
+			sliderTime.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent arg0) {
+					mediaplayer.seek(Duration.seconds(sliderTime.getValue()));
+					
 				}
 			});
 		}
+		
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -77,28 +114,58 @@ public class MySceneController implements Initializable {
 //            });
 //	
 		//sliderTime.setMaxWidth(Double.MAX_VALUE);
-		sliderTime.prefWidth(Double.MAX_VALUE);
 		
 	}
 	public void Play(ActionEvent event) {
-		mediapplayer.play();
+		mediaplayer.play();
 	}
 	public void Pause(ActionEvent event) {
-		mediapplayer.pause();
+		mediaplayer.pause();
 	}
 	public void Fast(ActionEvent event) {
 		rate+=0.25;
-		mediapplayer.setRate(rate);
+		mediaplayer.setRate(rate);
 	}
 	public void Slow(ActionEvent event) {
 		rate-=0.25;
-		mediapplayer.setRate(rate);
+		mediaplayer.setRate(rate);
 	}
 	public void Reload(ActionEvent event) {
-		mediapplayer.seek(mediapplayer.getStartTime());
-		mediapplayer.play();
+		mediaplayer.seek(mediaplayer.getStartTime());
+		mediaplayer.play();
 	}
-	
+	public String convertTime(String times) {
+		int hour=0, min, sec;
+		System.out.println(times);
+		String[] parts = times.split("\\.");
+		String temp = parts[0];
+		System.out.println(parts[0]);
+		int time = Integer.valueOf(temp);
+		String result = "";
+		if(time/3600 == 0) {
+			hour=0;
+			min = time / 60;
+			sec = time % 60;
+			
+		}
+		else {
+			hour=time / 3600;
+			time = time % 3600;
+			min = time/60;
+			sec= time % 60;
+		}
+		if(hour>0)
+			result+=String.valueOf(hour);
+		if(min>10)
+			result+=String.valueOf(min);
+		else
+			result+=":0"+String.valueOf(min);
+		if(sec>10)
+			result+=":"+String.valueOf(sec);
+		else
+			result+=":0"+String.valueOf(sec);
+		return result;
+	}
 //	public void VolumeUp(ActionEvent event) {
 //		mp.setVolume(slVolume.getValue()*100);
 //	}
